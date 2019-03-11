71bf0a34e4bdd6198e82630f3d0502449506a73f
ItzNatsuki <48438460+ItzNatsuki@users.noreply.github.com>
2019-03-11 14:57:05 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,14 +2,13 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('I am ready!');
+  console.log(`Logged in as ${ItzNatsuki}!`);
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
 });
 
-// THIS  MUST  BE  THIS  WAY
-client.login(NTU0NDczNjQ1NjgyOTgyOTEz.D2fUSQ.S_M5BGSRie3SFQmBJ47uTNgiMrQ);
+client.login('NTU0NDczNjQ1NjgyOTgyOTEz.D2dP3Q.DJf_7ikrUqeTUmCKEZXKzzRD5N0');
